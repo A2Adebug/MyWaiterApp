99ee7543f6fd6250ba601020d8865f9b0b4c9be5
Andrey Vlasyev <vlasyevandrei@gmail.com>
2021-07-21 21:42:50 +0300

    Обновление списка объявлений свайпом

--- app/src/main/java/com/example/mywaiterapp/AdvertList.kt
+++ app/src/main/java/com/example/mywaiterapp/AdvertList.kt
@@ -3,42 +3,66 @@ package com.example.mywaiterapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_advert_list.*
+import kotlin.random.Random
 
 class AdvertList : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_advert_list)
 
-        val db = FirebaseFirestore.getInstance()
 //        FirebaseFirestore.setLoggingEnabled(true);
 
         adListRecyclerView.adapter = AdvertListAdapter(mutableListOf())
-
-        db.collection("adverts").get()
-            .addOnSuccessListener { result ->
-                // да, я говнишка, и вложил мапу в лист, чтобы не париться
-                val dataSet: MutableList<MutableMap<String, String>> = arrayListOf()
-                for (advert in result) {
-                    val data = advert.data
-                    data["id"] = advert.id
-                    dataSet.add(data as MutableMap<String, String>)
-                }
-
-                val adapter = AdvertListAdapter(dataSet)
-
-                adListRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-                adListRecyclerView.adapter = adapter
-                adapter.notifyDataSetChanged()
-                Log.d("Barmaley", dataSet.toString())
-            }
+        refreshList()
 
         adListBtnNewAdvert.setOnClickListener {
             val intent = Intent(this, NewAdvert::class.java)
             startActivity(intent)
         }
+
+        swipeRefresh.setOnRefreshListener {
+            refreshList()
+        }
+    }
+
+    private fun refreshList() {
+        val db = FirebaseFirestore.getInstance()
+        db.collection("adverts").get()
+                .addOnSuccessListener { result ->
+                    // да, я говнишка, и вложил мапу в лист, чтобы не париться
+                    val dataSet: MutableList<MutableMap<String, String>> = arrayListOf()
+                    for (advert in result) {
+                        val data = advert.data
+                        data["id"] = advert.id
+                        dataSet.add(data as MutableMap<String, String>)
+                    }
+
+                    val adapter = AdvertListAdapter(dataSet)
+
+                    adListRecyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+                    adListRecyclerView.adapter = adapter
+                    adapter.notifyDataSetChanged()
+
+//                    Log.d("Barmaley", dataSet.toString())
+
+                    val runnable = Runnable {
+                        swipeRefresh.isRefreshing = false
+                    }
+
+                    Handler().postDelayed(
+                            runnable, 1500.toLong() // это чтобы анимация обновления подольше была
+                    )
+                }
+        swipeRefresh.setColorSchemeResources(
+                android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light
+        )
     }
 }
--- app/src/main/java/com/example/mywaiterapp/Authorization.kt
+++ app/src/main/java/com/example/mywaiterapp/Authorization.kt
@@ -1,11 +1,24 @@
 package com.example.mywaiterapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_authorization.*
+
 
 class Authorization : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authorization)
+
     }
 }
